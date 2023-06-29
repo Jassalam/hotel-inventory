90d79f1b8faa3163f7d7a7bee209e3d5c26c993d
JASVEER KAUR <jasveerk1980@gmail.com>
2023-06-29 13:43:51 +0200

    first commit

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hinvapp-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent {
+ hotelName = 'Hotel Hilton';
+
+}
