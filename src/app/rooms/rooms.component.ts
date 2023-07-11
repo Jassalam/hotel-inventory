c2b409296d8f5fd80bc14f4a8736b77aaade0158
JASVEER KAUR <jasveerk1980@gmail.com>
2023-07-11 14:28:32 +0200

    pipes

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -14,12 +14,42 @@ export class RoomsComponent {
  hideRooms = false;
 
  rooms: Room = {
-    availableRooms: 0,
+    availableRooms: 10,
     bookedRooms : 5,
     totalRooms: 20,
  }
 
  roomList: RoomList[] = [
+  {
+  roomNumber: 1,
+  roomType: 'Deluxe Room',
+  amenities: 'Air Conditioner, free wifi',
+  price: 500,
+  photos: 'https://unsplash.com/photos/VmAaEe0vVi0.jpeg',
+  checkinTime: new Date('11-Nov-2023'),
+  checkoutTime: new Date('13-Nov-2023'),
+  rating: 4.45757
+ },
+ {
+  roomNumber: 2,
+  roomType: 'Deluxe Room',
+  amenities: 'Air Conditioner, free wifi',
+  price: 1000,
+  photos: 'https://unsplash.com/photos/VmAaEe0vVi0.jpeg',
+  checkinTime: new Date('11-Nov-2023'),
+  checkoutTime: new Date('13-Nov-2023'),
+  rating: 3.0
+ },
+ {
+  roomNumber: 3,
+  roomType: 'Private Room',
+  amenities: 'Air Conditioner, free wifi',
+  price: 1500,
+  photos: 'https://unsplash.com/photos/VmAaEe0vVi0.jpeg',
+  checkinTime: new Date('11-Nov-2023'),
+  checkoutTime: new Date('13-Nov-2023'),
+  rating: 2.6
+ },
  ]
 
 
